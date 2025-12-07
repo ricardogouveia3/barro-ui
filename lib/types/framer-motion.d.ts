91c5b47a48c27006d54afa7b107ffe463e4150bd
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2025-12-07 20:04:43 -0300

    style: Reformat `tsconfig.json` and `framer-motion.d.ts` for consistent indentation.

--- lib/types/framer-motion.d.ts
+++ lib/types/framer-motion.d.ts
@@ -1,28 +1,28 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 declare module 'framer-motion' {
-    import * as React from 'react';
+  import * as React from 'react';
 
-    export interface MotionProps extends React.HTMLAttributes<HTMLElement> {
-        animate?: any;
-        initial?: any;
-        exit?: any;
-        transition?: any;
-        whileHover?: any;
-        whileTap?: any;
-        whileFocus?: any;
-        whileDrag?: any;
-        whileInView?: any;
-        variants?: any;
-        [key: string]: any;
-    }
+  export interface MotionProps extends React.HTMLAttributes<HTMLElement> {
+    animate?: any;
+    initial?: any;
+    exit?: any;
+    transition?: any;
+    whileHover?: any;
+    whileTap?: any;
+    whileFocus?: any;
+    whileDrag?: any;
+    whileInView?: any;
+    variants?: any;
+    [key: string]: any;
+  }
 
-    export const motion: {
-        [K in keyof JSX.IntrinsicElements]: React.ForwardRefExoticComponent<
-            MotionProps & JSX.IntrinsicElements[K]
-        >;
-    };
+  export const motion: {
+    [K in keyof JSX.IntrinsicElements]: React.ForwardRefExoticComponent<
+      MotionProps & JSX.IntrinsicElements[K]
+    >;
+  };
 
-    export function AnimatePresence(props: any): JSX.Element;
+  export function AnimatePresence(props: any): JSX.Element;
 }
 
-export { };
+export {};
