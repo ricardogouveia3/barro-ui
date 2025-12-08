d164e44932f8970bfa8c06cf42cd1fb1d4bb5071
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2025-12-08 08:44:33 -0300

    style: update Badge component's default styling, font size, padding, and `default` variant colors.

--- lib/components/Badge/Badge.variants.ts
+++ lib/components/Badge/Badge.variants.ts
@@ -1,12 +1,12 @@
 import { variants } from '../../utils/variants';
 
 export const badgeVariants = variants(
-  'inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-stone-400 focus:ring-offset-2',
+  'text-[12px] inline-flex items-center rounded-full default-border px-3 py-0.5 font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-stone-400 focus:ring-offset-2',
   {
     variants: {
       variant: {
         default:
-          'border-transparent bg-stone-900 text-stone-50 hover:bg-stone-900/80 dark:bg-stone-50 dark:text-stone-900 dark:hover:bg-stone-50/80',
+          'bg-transparent smooth-text-color hover:bg-stone-100 dark:hover:bg-stone-800',
         secondary:
           'border-transparent bg-stone-100 text-stone-900 hover:bg-stone-100/80 dark:bg-stone-800 dark:text-stone-50 dark:hover:bg-stone-800/80',
         destructive:
